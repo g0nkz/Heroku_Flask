d5a42b9b08b4543cde9d4325e823979c13053a7d
g0nkz <elgonkz@hotmail.es>
2022-02-17 13:18:20 -0600

    Firts commit.

---
+++ app.py
@@ -0,0 +1,30 @@
+from db import db
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from security import authenticate, identity
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'dev'
+api = Api(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Store, '/store/<string:name>')
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(host="0.0.0.0", port=5000, debug=True)
