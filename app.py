f2253d9167003b5961afb93f6d58ea8aee0a3405
g0nkz <elgonkz@hotmail.es>
2022-02-21 12:29:03 -0600

    DB fixed.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from db import db
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -12,10 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dev'
 api = Api(app)
-
-@app.before_first_request
-def create_table():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
